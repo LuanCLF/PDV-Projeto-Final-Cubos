390391adc9717a377f9336f8f3c5ffcf08859ce2
wwesleyg <wesley-george@hotmail.com>
2023-10-23 20:51:55 -0300

    feito logica e rota da parte de listar categorias.

--- src/rotas/rotas.js
+++ src/rotas/rotas.js
@@ -5,7 +5,9 @@ const {
   cadastrarUsuario,
 } = require("../controladores/usuarios/cadastrarUsuario");
 const { loginUsuario } = require("../controladores/usuarios/loginUsuario");
+const { listarCategorias } = require("../controladores/categorias/listarCategorias");
 
 rotas.post("/usuario", cadastrarUsuario);
 rotas.post("/login", loginUsuario);
+rotas.get("/categorias", listarCategorias)
 module.exports = rotas;
