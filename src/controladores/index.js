8f28c547fbb3d372ba00e218ce60b7d8fb8b3630
Luan Charlys <firminocharlys@gmail.com>
2023-10-23 16:59:28 -0300

    feat: login do usuario

--- src/controladores/index.js
+++
@@ -1,6 +0,0 @@
-const { cadastrarUsuario } = require("./usuarios/cadastrarUsuario");
-const { loginUsuario } = require("./usuarios/loginUsuario");
-
-const controladorUsuario = { cadastrarUsuario, loginUsuario };
-
-module.exports = { controladorUsuario };
--- src/rotas/rotas.js
+++ src/rotas/rotas.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const rotas = express();
 
-const { controladorUsuario } = require("../controladores/index");
+const {
+  cadastrarUsuario,
+} = require("../controladores/usuarios/cadastrarUsuario");
+const { loginUsuario } = require("../controladores/usuarios/loginUsuario");
 
-rotas.post("/usuario", controladorUsuario.cadastrarUsuario);
-rotas.post("/login", controladorUsuario.loginUsuario);
+rotas.post("/usuario", cadastrarUsuario);
+rotas.post("/login", loginUsuario);
 module.exports = rotas;
