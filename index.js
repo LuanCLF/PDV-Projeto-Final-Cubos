6a3579771e24a929aa289c932b947fa2ebd5dedf
Lucas Cezar <lucas.oheisen@gmail.com>
2023-10-23 12:49:59 -0300

    primeiro commit, adicionando estrutura no projeto.

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config
+const express = require('express')
+const rotas = require('./src/rotas/rotas')
+
+
+const app = express()
+
+app.use(express.json())
+
+app.use(rotas)
+
+app.listen(process.env.PORT,
+    console.log(`Servidor rodando na porta ${process.env.PORT}`))
---
+++ src/rotas/rotas.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const rotas = express()
+
+
+
+module.exports = rotas
