45578f1019082251de3a85f05d53084fe2eaa499
tenchini <viniciustenchi@gmail.com>
2023-10-24 12:51:17 -0300

    Criado intermediario de autenticação e validarRequisição via JOI.

---
+++ src/intermediarios/autenticacao.js
@@ -0,0 +1,41 @@
+const jwt = require("jsonwebtoken")
+const knex = require("../bancoDeDados/conexao")
+
+const hash = process.env.SENHA_JWT
+
+const autenticacao = async (req, res, next) => {
+    const { authorization } = req.headers
+
+    if (!authorization) {
+        return res.status(401).json({
+            mensagem: "Não autorizado",
+        })
+    }
+
+    try {
+        const token = authorization.replace("Bearer ", "").trim()
+
+        const { id } = jwt.verify(token, hash)
+
+        const usuarioExiste = await knex("usuarios").where({ id }).first()
+
+        if (!usuarioExiste) {
+            return res.status(404).json({
+                mensagem: "Usuario não encontrado",
+            })
+        }
+
+        const { senha, ...usuario } = usuarioExiste
+
+        req.usuario = usuario
+
+        next()
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            mensagem: "Erro interno do servidor.",
+        })
+    }
+}
+
+module.exports = autenticacao
---
+++ src/intermediarios/validarRequisicao.js
@@ -0,0 +1,13 @@
+const validarRequisicao = (joiSchema) => async (req, res, next) => {
+    try {
+        await joiSchema.validateAsync(req.body)
+        next()
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({
+            mensagem: error.message,
+        })
+    }
+}
+
+module.exports = validarRequisicao
