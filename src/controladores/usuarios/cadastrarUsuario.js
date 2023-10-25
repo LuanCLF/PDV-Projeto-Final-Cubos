d165fd359ed0273534a25a1cc3ea8ea856438c51
Lucas Cezar <lucas.oheisen@gmail.com>
2023-10-25 16:56:51 -0300

    controlador de cadastro refatorado

--- src/controladores/usuarios/cadastrarUsuario.js
+++ src/controladores/usuarios/cadastrarUsuario.js
@@ -1,22 +1,21 @@
 const { hash } = require("bcrypt");
 const knex = require("../../bancoDeDados/conexao");
+const { emailExistente, usuarioCadastrado } = require("../../bancoDeDados/queryFuncoes");
 
 const cadastrarUsuario = async (req, res) => {
   try {
     const { nome, email, senha } = req.body;
-    const seExiste = await knex("usuarios").select("id").where("email", email);
 
-    if (seExiste.length > 0) {
+    const EmailCadastrado = await emailExistente(email)
+
+    if (EmailCadastrado.length > 0) {
       return res.status(409).json({ mensagem: "Usuário já está cadastrado" });
     }
 
     const senhaCriptografada = await hash(senha, 10);
 
-    await knex("usuarios").insert({
-      nome,
-      email,
-      senha: senhaCriptografada,
-    });
+    await usuarioCadastrado(nome, email, senhaCriptografada)
+
     res.status(201).json();
   } catch (error) {
     res.status(500).json({ mensagem: "Erro interno no servidor" });
