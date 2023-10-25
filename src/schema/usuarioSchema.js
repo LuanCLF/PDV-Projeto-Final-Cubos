bfe28c644dbf38f37880d6c5577aba772faf79c3
Luan Charlys <firminocharlys@gmail.com>
2023-10-25 11:39:38 -0300

    refact: criei schema pra requisicao de login e cadastro, e fiz a separacao nos schemas, tbm ajustei o schema do banco de dados

--- src/schema/usuarioSchema.js
+++ src/schema/usuarioSchema.js
@@ -1,23 +1,29 @@
-const joi = require("joi")
+const joi = require("joi");
 
-const usuarioSchema = joi.object({
-    nome: joi.string().required().messages({
-        "any.required": "O campo nome é obrigatório.",
-        "string.empty": "O campo nome não pode estar vazio.",
-    }),
+const usuarioSchema = {
+  nome: joi.string().required().messages({
+    "any.required": "O campo nome é obrigatório.",
+    "string.empty": "O campo nome não pode estar vazio.",
+  }),
 
-    email: joi.string().email().required().unique().messages({
-        "any.required": "O campo email é obrigatório.",
-        "string.empty": "O campo email não pode estar vazio.",
-        "string.email": "O campo email deve ser um email válido.",
-        "array.unique": "O campo email não pode ser duplicado",
-    }),
+  email: joi.string().email().required().messages({
+    "any.required": "O campo email é obrigatório.",
+    "string.empty": "O campo email não pode estar vazio.",
+    "string.email": "O campo email deve ser um email válido.",
+    "array.unique": "O campo email não pode ser duplicado",
+  }),
 
-    senha: joi.string().min(3).required().messages({
-        "any.required": "O campo senha é obrigatório.",
-        "string.empty": "O campo senha não pode estar vazio.",
-        "string.min": "O campo senha deve ter, no mínimo, 3 caracteres.",
-    }),
-})
+  senha: joi.string().min(3).required().messages({
+    "any.required": "O campo senha é obrigatório.",
+    "string.empty": "O campo senha não pode estar vazio.",
+    "string.min": "O campo senha deve ter, no mínimo, 3 caracteres.",
+  }),
+};
 
-module.exports = usuarioSchema
+const cadastro = joi.object(usuarioSchema);
+const editar = joi.object(usuarioSchema);
+
+const { nome, ...schemaLogin } = usuarioSchema;
+const login = joi.object(schemaLogin);
+
+module.exports = { cadastro, editar, login };
