418080e5e0c6797b82fd982f096e73002a537c5d
Luan Charlys <firminocharlys@gmail.com>
2023-10-25 16:39:53 -0300

    feat: workflow/integracao continua do github actions criada

--- src/bancoDeDados/migrations/0002_criar_categorias.js
+++ src/bancoDeDados/migrations/0002_criar_categorias.js
@@ -1,18 +1,26 @@
 exports.up = function (knex) {
-  return knex.schema
-    .createTable("categorias", (table) => {
-      table.bigIncrements("id").primary().index();
-      table.string("descricao").checkLength("<=", 100).index().notNullable();
+  return knex.schema.hasTable("categorias").then(function (exists) {
+    if (!exists) {
+      return knex.schema
+        .createTable("categorias", function (table) {
+          table.bigIncrements("id").primary().index();
+          table
+            .string("descricao")
+            .checkLength("<=", 100)
+            .index()
+            .notNullable();
 
-      table.comment("tabela de categorias");
-    })
-    .then(() => {
-      console.log("# criou a tabela de categorias");
-    });
+          table.comment("tabela de categorias");
+        })
+        .then(() => {
+          console.log("# criou a tabela de categorias");
+        });
+    }
+  });
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTable("categorias").then(() => {
+  return knex.schema.dropTableIfExists("categorias").then(() => {
     console.log("# deletou a tabela de categorias");
   });
 };
