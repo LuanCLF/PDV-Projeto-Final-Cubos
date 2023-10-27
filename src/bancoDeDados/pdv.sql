107c46e41b57a0648543fa5aa112522f365c0b74
Lucas Cezar <lucas.oheisen@gmail.com>
2023-10-27 12:02:51 -0300

    adicionado schema de produtos e clientes

--- src/bancoDeDados/pdv.sql
+++ src/bancoDeDados/pdv.sql
@@ -25,3 +25,30 @@ VALUES
 ('Bebê'),
 ('Games')
 
+CREATE TABLE clientes (
+id SERIAL PRIMARY KEY NOT NULL,
+nome VARCHAR(100) NOT NULL,
+email VARCHAR(50) NOT NULL UNIQUE,
+cpf VARCHAR(11) NOT NULL UNIQUE,
+cep VARCHAR(8),
+rua VARCHAR(100),
+numero VARCHAR(10),
+bairro VARCHAR(100),
+cidade VARCHAR(100),
+estado VARCHAR(2)
+);
+
+
+CREATE TABLE produtos(
+id SERIAL PRIMARY KEY NOT NULL,
+descricao TEXT NOT NULL,
+quantidade_estoque INT NOT NULL,
+valor INT NOT NULL,
+categoria_id SERIAL REFERENCES categorias(id)
+)
+
+--produto teste
+---insert into produtos (descricao, quantidade_estoque, valor, categoria_id) 
+--values ('celular iphone apple', 3, 5000, 2)
+
+
