2a385d87221015b2d2e7bc04bc29564a45abfc49
tenchini <viniciustenchi@gmail.com>
2023-10-30 17:33:38 -0300

    Remoção de comentários.

--- src/controladores/clientes/detalharCliente/detalharCliente.js
+++ src/controladores/clientes/detalharCliente/detalharCliente.js
@@ -4,21 +4,6 @@ const {
     detalharClientes,
 } = require("../../../provedor/clientesQuerys/queryFuncoes")
 
-//tenchi quando for mexer, não precisa abrir trycatch, se estourar erro vai cair no middleware
-
-// pra atirar erros você faz assim:
-
-/*  if(talCoisa){
-  throw conflictseilaoq("mensagem")
-}  
-
-as funções que vc pode jogar estao no arquivo erros no helpers, é o api-erros, se n tiver oq vc precisa, é só criar um, se n conseguir criar chama um de nós
-
-ai pra usar os status code, é só importar a biblioteca StatusCodes e fazer assim
-
-res.status(StatusCodes.OK).json(cliente)
- */
-
 const detalharCliente = async (req, res) => {
     const { id } = req.params
 
