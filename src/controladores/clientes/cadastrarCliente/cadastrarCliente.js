1d789e0bfa3b83080ddd03c3bcaebaae9e48ff0a
tenchini <viniciustenchi@gmail.com>
2023-10-30 17:16:57 -0300

    função ConflictRequestError importada de maneira correta.

--- src/controladores/clientes/cadastrarCliente/cadastrarCliente.js
+++ src/controladores/clientes/cadastrarCliente/cadastrarCliente.js
@@ -1,25 +1,29 @@
 const {
-  clienteCadastrado,
-  emailCliente,
-  cpfCliente,
-} = require("../../../provedor/clientesQuerys/queryFuncoes");
-const { StatusCodes } = require("http-status-codes");
+    clienteCadastrado,
+    emailCliente,
+    cpfCliente,
+} = require("../../../provedor/clientesQuerys/queryFuncoes")
+const { StatusCodes } = require("http-status-codes")
+
+const {
+    ConflictRequestError,
+} = require("../../../helpers/erros/api-errors-helpers")
 
 const cadastrarCliente = async (req, res) => {
-  const { nome, email, cpf } = req.body;
+    const { nome, email, cpf } = req.body
 
-  const existeEmail = await emailCliente(email);
-  const existeCpf = await cpfCliente(cpf);
+    const existeEmail = await emailCliente(email)
+    const existeCpf = await cpfCliente(cpf)
 
-  if (existeEmail) {
-    throw ConflictRequestError("email ja existe");
-  }
-  if (existeCpf) {
-    throw ConflictRequestError("cpf ja existe");
-  }
+    if (existeEmail) {
+        throw ConflictRequestError("email ja existe")
+    }
+    if (existeCpf) {
+        throw ConflictRequestError("cpf ja existe")
+    }
 
-  await clienteCadastrado(nome, email, cpf);
-  res.status(StatusCodes.CREATED).json();
-};
+    await clienteCadastrado(nome, email, cpf)
+    res.status(StatusCodes.CREATED).json()
+}
 
-module.exports = { cadastrarCliente };
+module.exports = { cadastrarCliente }
