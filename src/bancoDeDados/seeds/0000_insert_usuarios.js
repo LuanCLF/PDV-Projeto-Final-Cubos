60618b12cafdeca42be35696bc63694d5946bbb6
Luan Charlys <firminocharlys@gmail.com>
2023-11-06 19:26:48 -0300

    tirei o teste que da b.o

--- src/bancoDeDados/seeds/0000_insert_usuarios.js
+++ src/bancoDeDados/seeds/0000_insert_usuarios.js
@@ -3,7 +3,6 @@ const bcrypt = require("bcrypt");
 exports.seed = async (knex) => {
   const senha = await bcrypt.hash("senha", 10);
   const inserirUsuarios = [
-    { nome: "testeTesteA", email: "testeTesteA@gmail.com", senha },
     { nome: "testeTesteB", email: "testeTesteB@gmail.com", senha },
     { nome: "testeTesteC", email: "testeTesteCadastro@teste.com", senha },
     { nome: "testeTesteD", email: "testeTesteLogin@teste.com", senha },
