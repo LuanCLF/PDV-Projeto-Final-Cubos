ddfdfdb7171b2f56b47a17e0cce98b32ef706b4f
Luan Charlys <firminocharlys@gmail.com>
2023-11-15 21:12:17 -0300

    feat: parte de editar pronta

--- src/controladores/clientes/cadastrarCliente.js
+++ src/controladores/clientes/cadastrarCliente.js
@@ -1,17 +1,11 @@
 const {
   clienteCadastrado,
-  emailCliente,
-  cpfCliente,
   checaSeClienteExiste,
 } = require("../../provedor/clientesQuerys/queryFuncoes");
 const { StatusCodes } = require("http-status-codes");
 
 const { ErroDeConflito } = require("../../uteis/erros/erroDaApi");
-const {
-  erroEmailExistente,
-  erroCpfExistente,
-  erroEmailOuCpfExistente,
-} = require("../../uteis/erros/mensagens");
+const { erroEmailOuCpfExistente } = require("../../uteis/erros/mensagens");
 
 const cadastrarCliente = async (req, res) => {
   const { nome, email, cpf } = req.body;
@@ -23,7 +17,7 @@ const cadastrarCliente = async (req, res) => {
   }
 
   await clienteCadastrado(nome, email, cpf);
-  
+
   res.status(StatusCodes.CREATED).json();
 };
 
